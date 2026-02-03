70f23a3a2aee3fe32fc1e32c03b182a577c87fac
skokado <terutacchi@gmail.com>
2026-02-03 20:49:06 +0900

    complete

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_lambda_function" "func_hello_world" {
 data "archive_file" "func_with_dep_src_zip" {
   type        = "zip"
   source_dir  = "${path.root}/func_with_dep/src"
-  output_path = "${path.root}/func_with_dep/archive.zip"
+  output_path = "${path.root}/func_with_dep/archive_src.zip"
 
   excludes = [
     "*.pyc",
@@ -56,30 +56,24 @@ data "archive_file" "func_with_dep_src_zip" {
   ]
 }
 
-resource "terraform_data" "func_with_dep_sam_build" {
-  triggers_replace = [
-    timestamp(),
-    data.archive_file.func_with_dep_src_zip.output_base64sha256,
-  ]
+# sam build および zip アーカイブ作成 (これが最終的に Lambda 関数で使われる)
+resource "terraform_data" "func_with_dep_sam_build_archive" {
+  triggers_replace = {
+    sha256 = data.archive_file.func_with_dep_src_zip.output_base64sha256
+  }
 
   provisioner "local-exec" {
+    when        = create
     command     = "sam build"
     working_dir = "${path.root}/func_with_dep"
   }
-}
 
-# sam build で生成された成果物の zip アーカイブ (これが最終的に Lambda 関数で使われる)
-data "archive_file" "func_with_dep_sam_build_zip" {
-  depends_on = [terraform_data.func_with_dep_sam_build]
-
-  type        = "zip"
-  source_dir  = "${path.root}/func_with_dep/.aws-sam/build/HelloWorldFunction"
-  output_path = "${path.root}/func_with_dep/archive_sam_build.zip"
-
-  excludes = [
-    "*.pyc",
-    "**/__pycache__/",
-  ]
+  # func_with_dep/archive.zip を作成する
+  provisioner "local-exec" {
+    when        = create
+    command     = "zip -r ../../../archive.zip ."
+    working_dir = "${path.root}/func_with_dep/.aws-sam/build/HelloWorldFunction"
+  }
 }
 
 resource "aws_lambda_function" "func_with_dep" {
@@ -87,6 +81,6 @@ resource "aws_lambda_function" "func_with_dep" {
   role             = aws_iam_role.lambda_exec.arn
   handler          = "app.lambda_handler"
   runtime          = "python3.14"
-  filename         = data.archive_file.func_with_dep_sam_build_zip.output_path
-  source_code_hash = data.archive_file.func_with_dep_sam_build_zip.output_base64sha256
+  filename         = "${path.root}/func_with_dep/archive.zip"
+  source_code_hash = terraform_data.func_with_dep_sam_build_archive.triggers_replace.sha256
 }
