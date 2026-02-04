d5a1a2cb9a9c6fd55316765be985875d1a9ef573
skokado <terutacchi@gmail.com>
2026-02-04 11:01:36 +0900

    fix: handler typo

--- main_hello_world.tf
+++ main_hello_world.tf
@@ -11,7 +11,7 @@ data "archive_file" "func_hello_world_src_zip" {
 resource "aws_lambda_function" "func_hello_world" {
   function_name    = "func_hello_world"
   role             = aws_iam_role.lambda_exec.arn
-  handler          = "app.index"
+  handler          = "app.lambda_handler"
   runtime          = "python3.14"
   filename         = data.archive_file.func_hello_world_src_zip.output_path
   source_code_hash = data.archive_file.func_hello_world_src_zip.output_base64sha256
